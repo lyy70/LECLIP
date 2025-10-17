c68b59aeec82204ab7d7bf50d770ab43d7b6adb4
Yuyao Liu <57789910+lyy70@users.noreply.github.com>
2025-10-17 16:22:01 +0800

    Add files via upload

---
+++ generate_dataset_json/DAGM.py
@@ -0,0 +1,41 @@
+import os
+import json
+
+class DAGMSolver(object):
+    CLSNAMES = [
+        'Class1','Class2','Class3','Class4','Class5','Class6','Class7','Class8','Class9','Class10',
+    ]
+
+    def __init__(self, root):
+        self.root = root
+        self.meta_path = f'{root}/meta.json'
+
+    def run(self):
+        info = dict(train={}, test={})
+        for cls_name in self.CLSNAMES:
+            cls_dir = f'{self.root}/{cls_name}'
+            for phase in ['train', 'test']:
+                cls_info = []
+                species = os.listdir(f'{cls_dir}/{phase}')#train/good-test/good/,test/defect/
+                for specie in species:
+                    is_abnormal = True if specie not in ['good'] else False
+                    img_names = os.listdir(f'{cls_dir}/{phase}/{specie}')
+                    mask_names = os.listdir(f'{cls_dir}/ground_truth/{specie}') if is_abnormal else None
+                    img_names.sort()
+                    mask_names.sort() if mask_names is not None else None
+                    for idx, img_name in enumerate(img_names):
+                        info_img = dict(
+                            img_path=f'{cls_name}/{phase}/{specie}/{img_name}',
+                            mask_path=f'{cls_name}/ground_truth/{specie}/{mask_names[idx]}' if is_abnormal else '',
+                            cls_name=cls_name,
+                            specie_name=specie,
+                            anomaly=1 if is_abnormal else 0,
+                        )
+                        cls_info.append(info_img)
+                info[phase][cls_name] = cls_info
+        with open(self.meta_path, 'w') as f:
+            f.write(json.dumps(info, indent=4) + "\n")
+
+if __name__ == '__main__':
+    runner = DAGMSolver(root='/media/LiuYuyao/Dataset/DAGM')
+    runner.run()
---
+++ generate_dataset_json/medical.py
@@ -0,0 +1,60 @@
+import os
+import json
+import pandas as pd
+
+import os
+import json
+
+class MedicalSolver(object):
+
+    CLSNAMES = [
+        # 'RESC',
+        # 'LiverCT',
+        # 'BrainMRI',
+        # 'Head_CT',
+        # 'Br35H',
+        # 'COVID-19',
+        # 'ISIC',
+        # 'Endo',
+        # 'TN3K',
+        #'Kvasir',
+        'CVC-ColonDB',
+        'CVC-ClinicDB',
+        # 'Esophagus'
+
+    ]
+
+    def __init__(self, root):
+        self.root = root
+        self.meta_path = f'{root}/meta.json'
+
+    def run(self):
+        info = dict(train={}, test={})
+        for cls_name in self.CLSNAMES:
+            cls_dir = f'{self.root}/{cls_name}'
+            for phase in ['train', 'test']:
+                cls_info = []
+                species = os.listdir(f'{cls_dir}/{phase}')#train/good-test/good/,test/defect/
+                for specie in species:
+                    is_abnormal = True if specie not in ['good'] else False
+                    img_names = os.listdir(f'{cls_dir}/{phase}/{specie}')
+                    mask_names = os.listdir(f'{cls_dir}/ground_truth/{specie}') if is_abnormal else None
+                    img_names.sort()
+                    mask_names.sort() if mask_names is not None else None
+                    for idx, img_name in enumerate(img_names):
+                        info_img = dict(
+                            img_path=f'{cls_name}/{phase}/{specie}/{img_name}',
+                            #image-level
+                            mask_path=f'{cls_name}/ground_truth/{specie}/{mask_names[idx]}' if is_abnormal else '',
+                            cls_name=cls_name,
+                            specie_name=specie,
+                            anomaly=1 if is_abnormal else 0,
+                        )
+                        cls_info.append(info_img)
+                info[phase][cls_name] = cls_info
+        with open(self.meta_path, 'w') as f:
+            f.write(json.dumps(info, indent=4) + "\n")
+
+if __name__ == '__main__':
+    runner = MedicalSolver(root='/data/LiuYuyao/Dataset/mvtec_colon/')
+    runner.run()
